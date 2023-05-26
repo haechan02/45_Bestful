36f18abf498c93ed6c73f80ae6704619e2099dc8
hecyoon <84567556+hecyoon@users.noreply.github.com>
2023-05-26 20:44:59 +0900

    FINAL

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -7,8 +7,8 @@ const { upload } = require('../utils/s3')
 const router = express.Router();
 
 router.post('/kakaologin', userController.signInKakao);
-router.get('/', userController.getUserById);
-router.patch('/edit', userController.editUserInfo);
+router.get('/', checkLogInToken, userController.getUserById);
+router.patch('/edit', checkLogInToken, userController.editUserInfo);
 router.post('/image', checkLogInToken, upload.single("profileImage"), userController.uploadImageUrl)
 router.get('/:userId', userController.getOtherUser)
 
