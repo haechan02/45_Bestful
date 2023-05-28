2c694573908455dce4a142c75a22f1e089d5f5bc
DoorWinBell <msj102525@gmail.com>
2023-05-29 01:54:53 +0900

    최종본

--- src/controllers/feedController.js
+++ src/controllers/feedController.js
@@ -2,15 +2,27 @@ const feedService = require('../services/feedService');
 const { catchAsync } = require('../utils/error');
 
 const getAllFeed = catchAsync(async (req, res) => {
-  const { from, count, genderId, seasonId, styleId } = req.query;
+  const { from, count, genderId, seasonId, styleId, orderBy } = req.query;
+  const { feedId, targetUserId, selectedUserId } = req.params;
 
-  const DEFAULT_LIMIT = 6;
+  const DEFAULT_LIMIT = 3;
+
   const DEFAULT_OFFSET = 0;
 
   const offset = from ? from : DEFAULT_OFFSET;
   const limit = count ? count : DEFAULT_LIMIT;
 
-  const result = await feedService.getAllFeed(offset, limit, genderId, seasonId, styleId);
+  const result = await feedService.getAllFeed(
+    feedId,
+    targetUserId,
+    selectedUserId,
+    offset,
+    limit,
+    genderId,
+    seasonId,
+    styleId,
+    orderBy
+  );
 
   return res.status(200).json(result);
 });
